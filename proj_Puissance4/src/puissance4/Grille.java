a65b05658999710c9f9a897272350061e0cf7d6b
EwaldJa <ewald.ja@users.noreply.github.com>
2018-12-18 17:54:47 +0100

    Finalisation des modifs pour Pion
    La grille est désormais un tableau matriciel de Pion, et les constructeurs pas défaut sont fait.

--- proj_Puissance4/src/puissance4/Grille.java
+++ proj_Puissance4/src/puissance4/Grille.java
@@ -2,10 +2,26 @@ package puissance4;
 
 public class Grille {
 	
+	
 	private Pion[][] grille;
 	public static int NB_LIGNE;
 	public static int NB_COLONNE;
+	public Vue mavue;
 	
+	
+	
+    public Grille(int nbColonne, int nbLigne, Vue vue) {
+    	NB_LIGNE = nbLigne;
+    	NB_COLONNE = nbColonne;
+        grille = new Pion[NB_LIGNE][NB_COLONNE];
+        for (int i = 0; i < NB_LIGNE; i++) {
+            for (int j = 0; j < NB_COLONNE; j++) {
+            	grille[i][j] = null;
+            }
+        }
+        mavue = vue;
+    }
+    
     public Grille(int nbColonne, int nbLigne) {
     	NB_LIGNE = nbLigne;
     	NB_COLONNE = nbColonne;
@@ -15,6 +31,19 @@ public class Grille {
             	grille[i][j] = null;
             }
         }
+        mavue = new VueConsole();
+    }
+    
+    public Grille(Vue vue) {
+    	NB_LIGNE = 6;
+    	NB_COLONNE = 7;
+        grille = new Pion[NB_LIGNE][NB_COLONNE];
+        for (int i = 0; i < NB_LIGNE; i++) {
+            for (int j = 0; j < NB_COLONNE; j++) {
+            	grille[i][j] = null;
+            }
+        }
+        mavue = vue;
     }
     
     public Grille() {
@@ -26,10 +55,40 @@ public class Grille {
             	grille[i][j] = null;
             }
         }
+        mavue = new VueConsole();
     }
+    
+    
     
     public Pion getPion(int indLigne, int indCol) throws IndexOutOfBoundsException{
     	return grille[indLigne][indCol];
+    }
+    
+    
+    public int[] placerPion(int col, Pion pion) throws Exception {
+    	if (col >= NB_COLONNE) {
+    		throw new IndiceIncorrectException();
+    	}
+    	else {
+    		int i = NB_LIGNE;
+    		while(i>=0) {
+    			if (grille[i][col] == null) {
+    				grille[i][col] = pion;
+    				return (new int[] {i,col});
+    			}
+    		}
+    		throw new ColonnePleineException();
+    		return null;
+    	}
+    }
+    
+    
+    public void affichage() {
+    	mavue.affichage(grille);
+    }
+    
+    public Pion[][] getGrille(){
+    	return grille;
     }
 	
 	/*

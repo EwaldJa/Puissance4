fabffbc225df37d7eb342d5eac772e6d01231b5f
EwaldJa <ewald.ja@users.noreply.github.com>
2018-12-18 17:58:41 +0100

    Légères adaptations des Vue

--- proj_Puissance4/src/puissance4/VueConsole.java
+++ proj_Puissance4/src/puissance4/VueConsole.java
@@ -2,7 +2,7 @@ package puissance4;
 
 public class VueConsole extends Vue {
 
-	public void affichage(Grille grille) {
+	public void affichage(Pion[][] grille) {
 		for (int i = 0; i < Jeu.MAX_LIGNES; i++) {
 			System.out.println(grille.getTabPionIndice(i).toString());
 		}
